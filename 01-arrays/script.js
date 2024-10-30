f2f11856b0fd20716bf84093a9a5ee0a2c3109f5
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-10-30 13:27:35 +0100

    es5

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -26,6 +26,8 @@ const firstTeacher = teachers.shift();
 console.log(firstTeacher);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push("Vanessa");
+console.dir(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
