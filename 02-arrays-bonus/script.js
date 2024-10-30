e315a837ffe168996fb6fe2800e44f627218aebd
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-10-30 14:09:24 +0100

    output + leggibile

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,7 +10,7 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-
+console.log("es1");
 const reversedTeachers = teachers.toReversed();
 console.log("toReversed() method:\n" + reversedTeachers);
 
@@ -18,12 +18,13 @@ const reversedCustom = [];
 for (let i = teachers.length - 1; i >= 0; i--) {
   reversedCustom.push(teachers[i]);
 }
-console.log("Without toReversed():\n" + reversedCustom);
+console.log("without toReversed():\n" + reversedCustom);
 
 
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
+console.log("\nes2");
 const longNames = teachers.filter(longname => longname.length >= 5);
 console.log("filter() method:\n" + longNames);
 
@@ -33,21 +34,19 @@ for (let i = 0; i < teachers.length; i++) {
     longCustom.push(teachers[i]);
   }
 }
-console.log("Without filter():\n" + longNames);
+console.log("without filter():\n" + longNames);
 
 
 // 3. Rimuovi 'Ed' dall'array teachers
+console.log("\nes3");
 const teachersCopy = teachers.slice();
-console.dir(teachersCopy);
-
 const indexEd = teachers.indexOf("Ed");
-if (indexEd !== -1) {
-  teachers.splice(indexEd, 1);
-}
+// assumendo che Ed sia elemento di teachers => index !== -1
+teachers.splice(indexEd, 1);
 console.log("splice() method teachers:\n" + teachers);
 
-for(let i=indexEd; i<teachersCopy.length - 1; i++){
-  teachersCopy[i] = teachersCopy[i+1];
+for (let i = indexEd; i < teachersCopy.length - 1; i++) {
+  teachersCopy[i] = teachersCopy[i + 1];
 }
 teachersCopy.pop();
-console.dir("without splice() teachersCopy:\n" + teachersCopy);
+console.log("without splice() teachersCopy:\n" + teachersCopy);
