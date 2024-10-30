08bded480957a2e2f57da6188e056a14bdf0dd66
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-10-30 13:47:39 +0100

    es1 bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,7 +10,17 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+
+const reversedTeachers = teachers.toReversed();
+console.log("Metodo toReversed() " + reversedTeachers);
+
+const reversedCustom = [];
+for(let i=teachers.length-1; i>=0; i--){
+  reversedCustom.push(teachers[i]);
+}
+console.log("Metodo \"fatto a mano\": " + reversedCustom);
+
+
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
