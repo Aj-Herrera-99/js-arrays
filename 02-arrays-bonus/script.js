cb1d30d5c6a65bdc5e4e92dce1d9e5f6af883254
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-10-30 13:58:08 +0100

    output + leggibile, aggiunto codice in es2 bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -12,19 +12,28 @@ const teachers = [
 // e salva il risultato nella variabile reversedTeachers
 
 const reversedTeachers = teachers.toReversed();
-console.log("Metodo toReversed() " + reversedTeachers);
+console.log("toReversed() method:\n" + reversedTeachers);
 
 const reversedCustom = [];
 for(let i=teachers.length-1; i>=0; i--){
   reversedCustom.push(teachers[i]);
 }
-console.log("Metodo \"fatto a mano\": " + reversedCustom);
+console.log("Without toReversed():\n" + reversedCustom);
 
 
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 const longNames = teachers.filter(longname => longname.length >= 5);
-console.log("longNames: " + longNames);
+console.log("filter() method:\n" + longNames);
+
+const longCustom = [];
+for(let i=0; i<teachers.length; i++){
+  if(teachers[i].length >= 5){
+    longCustom.push(teachers[i]);
+  }
+}
+console.log("Without filter():\n" + longNames);
+
 
 // 3. Rimuovi 'Ed' dall'array teachers
