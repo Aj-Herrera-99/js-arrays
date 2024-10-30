a86268c668a0af4af9239a4dd9740b169895f181
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-10-30 13:50:20 +0100

    es2 bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -24,6 +24,7 @@ console.log("Metodo \"fatto a mano\": " + reversedCustom);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = teachers.filter(longname => longname.length >= 5);
+console.log("longNames: " + longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
